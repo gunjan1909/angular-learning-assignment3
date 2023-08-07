7fea64510f1ba4d07eba7872b23b0f507d13c071
gunjan1909 <bhanarkaranita@gmail.com>
2023-08-07 20:41:42 +0530

    44

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'assignment3';
+  title: string = 'Assignment 3';
+  displayButton: boolean = false;
+  logs: Array<number> = [];
+
+  buttonClicked(): void {
+    this.displayButton = !this.displayButton;
+    this.logs.push(this.logs.length + 1);
+  }
 }
